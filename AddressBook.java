fcbaf275963215f2acc49e8d8e144678ae78f68a
GhaziNk <78899027+GhaziNk@users.noreply.github.com>
2021-02-15 22:18:35 -0800

    Add files via upload

---
+++ AddressBook.java
@@ -0,0 +1,30 @@
+
+
+public class AddressBook {
+
+    public static void main(String[] args)
+
+    {
+
+//calling functions from Menu
+
+        menuenu.prompt_FirstName("Ghazi");
+
+        Menu.prompt_LastName("noseir");
+
+        Menu.prompt_Street("6745 Leona ");
+
+        Menu.prompt_City("Oakland");
+
+        Menu.prompt_State("Ca");
+
+        Menu.prompt_Zip("94621");
+
+        Menu.prompt_Telephone("5102282201");
+
+        Menu.prompt_Email("101@gmail.com");
+
+    }
+
+}
+
